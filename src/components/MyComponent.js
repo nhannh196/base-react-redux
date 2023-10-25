8e681def6d4ebb60637c34280fc7d1c87314f09f
Nguyen Hong Nhan <nhannh1906@gmail.com>
2023-10-25 16:26:35 +0700

    20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
-    state={
-        name:'Nhan',
-        address:'Vinh Long',
+    state = {
+        name: 'Nhan',
+        address: 'Vinh Long',
+        age: 26
     }
 
-    handeClick(e){
-        console.log(e)
+    handeClick(e) {
+        // console.log(e)
+        this.setState({
+            name: "Hong Nhan",
+            age: Math.floor((Math.random() * 1000) + 1)
+        })
     }
 
-    handeMouseOver(e){
-        console.log(e.pageX, e.pageY)
+    handeMouseOver(e) {
+        // console.log(e.pageX, e.pageY)
     }
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
-                <button onClick={this.handeClick}>Click</button>
+                My name is {this.state.name} and I'm {this.state.age}
+                <button onClick={(e) => { this.handeClick(e) }}>Click</button>
                 <button onMouseOver={this.handeMouseOver}>Hover</button>
             </div>
         )
