ca2471f90df3c33f06a448e27e75d1f34bb94a67
Nguyen Hong Nhan <nhannh1906@gmail.com>
2023-10-25 15:57:31 +0700

    19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,10 +5,20 @@ class MyComponent extends React.Component {
         name:'Nhan',
         address:'Vinh Long',
     }
+
+    handeClick(e){
+        console.log(e)
+    }
+
+    handeMouseOver(e){
+        console.log(e.pageX, e.pageY)
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handeClick}>Click</button>
+                <button onMouseOver={this.handeMouseOver}>Hover</button>
             </div>
         )
     }
