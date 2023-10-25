4d4e72b5f0f61a945b3f962e8507950ddd1576f5
Nguyen Hong Nhan <nhannh1906@gmail.com>
2023-10-25 16:53:26 +0700

    21

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,23 +7,37 @@ class MyComponent extends React.Component {
         age: 26
     }
 
-    handeClick(e) {
-        // console.log(e)
+    // handeClick(e) {
+    //     // console.log(e)
+    //     this.setState({
+    //         name: "Hong Nhan",
+    //         age: Math.floor((Math.random() * 1000) + 1)
+    //     })
+    // }
+
+    // handeMouseOver(e) {
+    //     // console.log(e.pageX, e.pageY)
+    // }
+
+    handleOnChangeInput = (e) => {
         this.setState({
-            name: "Hong Nhan",
-            age: Math.floor((Math.random() * 1000) + 1)
+            name: e.target.value
         })
     }
-
-    handeMouseOver(e) {
-        // console.log(e.pageX, e.pageY)
+    handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
     }
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm {this.state.age}
-                <button onClick={(e) => { this.handeClick(e) }}>Click</button>
-                <button onMouseOver={this.handeMouseOver}>Hover</button>
+                {/* <button onClick={(e) => { this.handeClick(e) }}>Click</button>
+                <button onMouseOver={this.handeMouseOver}>Hover</button> */}
+                <form onSubmit={(e) => { this.handleOnSubmit(e) }}>
+                    <input type="text" onChange={(e) => { this.handleOnChangeInput(e) }} />
+                    <input type='submit' />
+                </form>
             </div>
         )
     }
