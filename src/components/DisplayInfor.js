577887cfd4236ff14687cba3f08ba950a77239d1
Nguyen Hong Nhan <nhannh1906@gmail.com>
2023-10-26 02:05:39 +0700

    28

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./DisplayInfor.scss"
+import logo from "../logo.svg"
 
 class DisplayInfor extends React.Component {
     state = {
@@ -14,7 +16,7 @@ class DisplayInfor extends React.Component {
         const { listUsers } = this.props;
 
         return (
-            <div>
+            <div className="display-infor-container">
                 <br />
                 <div>
                     <button onClick={() => { this.handleShowHide() }}>
@@ -37,6 +39,7 @@ class DisplayInfor extends React.Component {
                         })}
                     </div>
                 }
+                <img src={logo} />
             </div>
         )
     }
